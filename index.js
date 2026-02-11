a9613c65e27125b7b943f3f6f4b67e8d8401f4e1
Ngurah Ariwhrihaspati <ari.microsoft.hp@gmail.com>
2026-02-11 20:39:45 +0800

    restrict Google API Key and Oauth2 from GCP

--- index.js
+++ index.js
@@ -160,14 +160,14 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/auth/google/secrets",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
       try {
         console.log("Google profile:", profile);
         const result = await db.query("SELECT * FROM users WHERE email = $1", [
-          profile.email,
+          profile.emails[0].value,
         ]);
         if (result.rows.length === 0) {
           const newUser = await db.query(
